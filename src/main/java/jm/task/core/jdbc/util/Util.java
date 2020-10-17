bc599e0f4733ac179a31d54e24df2ff36c737b8b
Maxim Serov <maxim58r@gmail.com>
2020-10-17 21:54:27 +0300

    Start project 1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,30 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    public Util() throws SQLException {
+
+
+    }
+
+    static Connection getConnection() {
+        try {
+            DriverManager.registerDriver(new Driver());
+            String DB_TYPE = "jdbc:mysql://localhost:3306/users_db?";
+            String login = "max&";
+            String password = "serov";
+
+            return DriverManager.getConnection(DB_TYPE,login,password);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
